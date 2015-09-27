055f5addc47ef76e1185ec014d6b43cfda84a5b3
petteramu <Petteramu@gmail.com>
2015-09-27 23:46:59 +0200

    Banner and mobile fixes
    Fixed the banner to be an individual template, being positioned using
    flex instead of bad absolute positioning.
    
    Also fixed the mobile versions of the site regarding problems on
    extra-small and small devices.

--- app/routes/currentgame.js
+++ app/routes/currentgame.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     renderTemplate: function() {
+        this.render('banner', {
+            outlet: 'banner'
+        });
+        
         this.render('currentgame', {
             controller: 'currentgame'
         });
